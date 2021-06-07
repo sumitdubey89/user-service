7f0734c9bcedd556cd098dff11050099ddfcd8b0
egrc3uh <sumit.dubey@mckesson.com>
2021-06-07 11:17:05 +0530

    week1 more

--- src/main/java/epam/demo/userservice/controller/UserController.java
+++ src/main/java/epam/demo/userservice/controller/UserController.java
@@ -2,20 +2,48 @@ package epam.demo.userservice.controller;
 
 import java.util.Collections;
 import java.util.List;
+import javax.jws.soap.SOAPBinding;
 import epam.demo.userservice.model.User;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import epam.demo.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping("/{userId}")
-	public List<User> getUser(@PathVariable("userId") String userId){
-		return Collections.singletonList(new User("Kamal",23,2019));
+	@Autowired
+	UserRepository userRepository;
 
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userRepository.findAll();
 	}
 
+	@GetMapping("/users/{user_id}")
+	public List<User> getUserDetail(@PathVariable("user_id") Integer userId){
+		return userRepository.findAllById(Collections.singleton(userId));
+	}
+
+	@PostMapping("/users")
+	public String saveUserDetails(@RequestBody final User user){
+		userRepository.save(user);
+		return ("The entry for the User is saved as follows : "+user.toString());
+	}
+
+	@DeleteMapping("/users/{user_id}")
+	public String deleteUserEntry(@PathVariable("user_id") Integer userID){
+		userRepository.deleteById(userID);
+		return ("The User entry for ID :"+userID+" has been deleted");
+	}
+
+	@PutMapping("/users/{user_id}")
+	public String updateUserEntry(@PathVariable("user_id") Integer userId,@RequestBody final User user){
+			if(userId.equals(user.getId())){
+				userRepository.save(user);
+				return ("Entry for the user Id: "+userId+" has been updated");
+			}
+			return ("mismatch in userID in path and body");
+	}
 }
--- src/main/java/epam/demo/userservice/model/User.java
+++ src/main/java/epam/demo/userservice/model/User.java
@@ -1,23 +1,68 @@
 package epam.demo.userservice.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
 
-	private String name;
+
+	@Id
+	@Column(name = "user_id",nullable = false)
+	private Integer id;
+
+	@Column(name = "first_name",nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name",nullable = false)
+	private String lastName;
+
+	@Column(name = "age",nullable = false)
 	private int age;
+
+	@Column(name = "member_since",nullable = false)
 	private int memberSince;
 
-	public User(String name, int age, int memberSince) {
-		this.name = name;
+	@Column(name = "city_name",nullable = false)
+	private String addressCityName;
+
+	public User() {
+	}
+
+	public User(Integer id, String firstName, String lastName, int age, int memberSince, String addressCityName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.age = age;
 		this.memberSince = memberSince;
+		this.addressCityName = addressCityName;
 	}
 
-	public String getName() {
-		return name;
+	public Integer getId() {
+		return id;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public int getAge() {
@@ -34,5 +79,18 @@ public class User {
 
 	public void setMemberSince(int memberSince) {
 		this.memberSince = memberSince;
+	}
+
+	public String getAddressCityName() {
+		return addressCityName;
+	}
+
+	public void setAddressCityName(String addressCityName) {
+		this.addressCityName = addressCityName;
+	}
+
+	@Override public String toString() {
+		return "User{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", age=" + age + ", memberSince="
+				+ memberSince + ", addressCityName='" + addressCityName + '\'' + '}';
 	}
 }
